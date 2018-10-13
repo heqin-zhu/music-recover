659e30162cc614e80cf63a286de66b300576f1af
aaron <johndoe@example.com>
2018-10-13 21:47:31 +0800

    [+] MP3 嵌入歌词

--- README.md
+++ README.md
@@ -1,64 +1,34 @@
+
+# forked from mbinary/netease-cached-music
+[+] MP3 嵌入歌词
+
 # 网易云音乐缓存文件 --> MP3
 >网易云音乐的缓存文件经过处理,也就是异或加密了的,要获得MP3文件,需要解密. 这个仅用于学习. 请大家尊重音乐的版权.其实大多数歌曲都是可以下载的
-# 注意
-最新更新在 github 上, 博客很少更新, 所以如果你是从博客来的,
-请再读一遍下面的 README 
-
-![warning](http://ounix1xcw.bkt.clouddn.com/warning.png)
-
-（学习使用异或加密 :see_no_evil: )
-
-## 介绍
-这是一个用于获取网易云音乐缓存文件的mp3文件的 python script.
-
-利用缓存文件,解密得到MP3文件, 并通过其metadata,命名文件,顺便从api或者网页抓取歌词,详细介绍可以看[这里](https://mbinary.coding.me/decrypt-netease-music.html) 
 
 ## 需求
 * python3
 * python 模块
   - requests
-  - mutagen (optional)
-  
+  - mutagen
+
 可以pip3 install 安装
 
 ## 使用
+将缓存目录拖动到 decrypt.bat 即可, 例 `D:\CloudMusic\Cache`
 
 ### 获得缓存文件
 下面两种方法都行
 * 手机上的在 `netease/cloudmusic/Cache/Music1`里,将其复制到电脑上
 * 或者在电脑上 `**/cloudmusicdata/Cache`.
 
-最终得到的路径记为 `MUSIC`
+最终得到的路径为当前目录下 `网易云音乐缓存`
 
 ### 运行
 下面两种方法都行
 * 在命令行模式下
-`python3 decrypt.py $MUSIC`
+`python3 decrypt.py {MUSIC}`
 
-这里的 `MUSIC`  就是缓存文件（包含`.uc` 或 `.uc!` 文件）的地址
-
-* 复制此脚本到缓存文件的父目录,要求缓存文件命名为`Music1`, 然后直接运行脚本
-
-我在 这个 repo 里面上传了几个缓存文件,可以作为测试,在 [Music1](Music1) 中
-
-## 展示
-这是 gif 
-![](src/display.gif)
-
-## 结果
- 你就可以到缓存文件目录的父目录下 看到 `cached_网易云音乐`, 以及其中的 `lyric`, `music` :smiley: 
-
-* 运行输出
- ![](src/result.jpg)
-
-* 歌词
- ![](src/lyric.jpg)
-
-* MP3
- ![](src/music.jpg)
-
-## 贡献
-欢迎 fork & PR
+这里的 `MUSIC`  就是缓存目录（包含`.uc` 或 `.uc!` 文件）的地址, 例 `D:\CloudMusic\Cache`
 
 ## Licence
 [MIT](LICENCE)
--- decrypt.py
+++ decrypt.py
@@ -3,15 +3,26 @@ import os
 import sys
 import glob
 import requests
+import mutagen
 
-DESDIR = '../cached_网易云音乐'
-LRCDIR = os.path.join(DESDIR, 'lyric')
-MSCDIR = os.path.join(DESDIR, 'music')
+from mutagen.id3 import ID3, APIC, TIT2, TPE1, TRCK, TALB, USLT, error
+# ID3 info:
+# APIC: picture
+# TIT2: title
+# TPE1: artist
+# TRCK: track number
+# TALB: album
+# USLT: lyric
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+MSCDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '网易云音乐缓存')
 
 API = 'https://api.imjad.cn/cloudmusic/?'
 # two args: id  type
 # type=song, lyric, comments, detail, artist, album, search
-# eg  API = 'https://api.imjad.cn/cloudmusic/?type=song&id=1234132'    download music
+# eg  API = 'https://api.imjad.cn/cloudmusic/?type=song&id=537854958'    download music
 
 hasModu = False
 try:
@@ -35,15 +46,12 @@ class netease_music:
             path = input('input the path of cached netease_music')
         self.path = path
         safeprint('[+] Current Path: ' + path)
+        safeprint('[+] Output Path: ' + MSCDIR)
         os.chdir(path)
         self.files = glob.glob('*.uc') + glob.glob('*.uc!')
         self.id_mp = {}
         for i in self.files:
             self.id_mp[self.getId(i)] = i
-        if not os.path.exists(DESDIR):
-            os.mkdir(DESDIR)
-        if not os.path.exists(LRCDIR):
-            os.mkdir(LRCDIR)
         if not os.path.exists(MSCDIR):
             os.mkdir(MSCDIR)
         # import re
@@ -60,6 +68,7 @@ class netease_music:
         dic['artist'] = [info['ar'][0]['name']]
         dic['title'] = [info['name']]
         dic['cover'] = [info['al']['picUrl']]
+        dic['album'] = [info['al']['name']]
         return dic
 
     def getInfoFromFile(self, path):
@@ -72,7 +81,7 @@ class netease_music:
             print('[Error] You can use pip3 to install mutagen or connet to the Internet')
             raise Exception('Failed to get info of ' + path)
 
-    def getPath(self, dic,musicId):
+    def getPath(self, dic, musicId):
         title = dic['title'][0]
         artist = dic['artist'][0]
         if artist in title:
@@ -89,30 +98,31 @@ class netease_music:
         musicId = self.getId(cachePath)
         idpath = os.path.join(MSCDIR, musicId + '.mp3')
         try:  # from web
-            dic = self.getInfoFromWeb(musicId)
-            path = self.getPath(dic,musicId)
-            if os.path.exists(path): return 
-            with open(path,'wb') as f:
-                f.write(bytes(self._decrypt(cachePath)))
+            info = self.getInfoFromWeb(musicId)
+            path = self.getPath(info, musicId)
+            if not os.path.exists(path):
+                with open(path,'wb') as f:
+                    f.write(bytes(self._decrypt(cachePath)))
         except Exception as e:  # from file
             print(e)
             if not os.path.exists(idpath):
                 with open(idpath,'wb') as f:
                     f.write(bytes(self._decrypt(cachePath)))
-            dic = self.getInfoFromFile(idpath)
-            path = getPath(dic,musicId)
+            info = self.getInfoFromFile(idpath)
+            path = getPath(info, musicId)
             if os.path.exists(path):
                 os.remove(idpath)
                 return 
             os.rename(idpath, path)
-            
+        return info, path
+
     def _decrypt(self,cachePath):
         with open(cachePath, 'rb') as f:
             btay = bytearray(f.read())
         for i, j in enumerate(btay):
             btay[i] = j ^ 0xa3
         return btay
-    
+
     def getLyric(self, musicId):
         name = self.id_mp[musicId]
         url = API + 'type=lyric&id=' + musicId
@@ -120,23 +130,44 @@ class netease_music:
         try:
             lrc = requests.get(url).json()['lrc']['lyric']
             if lrc=='':
-                lrc = requests.get(url2).json()['lrc']['lyric']
+                lrc = requests.get(url2, verify=False).json()['lrc']['lyric']
             if lrc=='':
                 raise Exception('')
-            file = os.path.join(LRCDIR, name + '.lrc')
-            if not os.path.exists(file):
-                with open(file, 'w', encoding='utf8') as f:
-                    f.write(str(lrc))
+
+            return lrc
+            # write lrc file
+            # LRCDIR = os.path.join(MSCDIR, 'lyric')
+            # if not os.path.exists(LRCDIR):
+            #     os.mkdir(LRCDIR)
+            # file = os.path.join(LRCDIR, name + '.lrc')
+            # if not os.path.exists(file):
+            #     with open(file, 'w', encoding='utf8') as f:
+            #         f.write(str(lrc))
         except Exception as e:
-            print(e,end='')
-            safeprint(': Failed to get lyric of music '+name)
+            safeprint('{} Failed to get lyric of music. Line {}: {}'.format(name, format(sys.exc_info()[-1].tb_lineno), e))
+
+    def setID3(self, lrc, info, path):
+        tags = ID3(path)
+        # remove old unsychronized lyrics
+        if len(tags.getall("USLT")) != 0:
+            tags.delall("USLT")
+
+        tags.add(TALB(encoding=3, lang='', desc='', text=info['album'][0]))
+        tags.add(TIT2(encoding=3, lang='', desc='', text=info['title'][0]))
+        tags.add(TPE1(encoding=3, lang='', desc='', text=info['artist'][0]))
+
+        for key,values in tags.items():
+            print('\t', key, ': ', values, sep='')
+
+        tags.add(USLT(encoding=3, lang='eng', desc='aaa', text=lrc))
+        tags.save()
+
     def getMusic(self):
         for ct, cachePath in enumerate(self.files):
-            self.decrypt(cachePath)
+            info, path = self.decrypt(cachePath)
             musicId = self.getId(cachePath)
             print('[{}]'.format(ct+1).ljust(5)+self.id_mp[musicId])
-            self.getLyric(musicId)
-
+            self.setID3(self.getLyric(musicId), info, path)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
