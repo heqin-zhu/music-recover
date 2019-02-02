f59587b0ebad09ce248eb86e9c96738951e17c72
mbinary <zhuheqin1@gmail.com>
2019-02-02 10:25:00 +0800

    Remove config

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DESDIR = 'cached_网易云音乐'
-LRCDIR = os.path.join(DESDIR, 'lyric')
-MSCDIR = os.path.join(DESDIR, 'music')
-
-API = 'https://api.imjad.cn/cloudmusic/?'
-
-SRCDIR = os.path.join(os.getcwd(),'Music1')
