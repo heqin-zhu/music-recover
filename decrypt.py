6d10fa8d50fa8a7d5b547d3b5afceb1292486b09
mbinary <zhuheqin1@gmail.com>
2019-04-10 17:34:13 +0800

    Update setid3

--- decrypt.py
+++ decrypt.py
@@ -12,7 +12,8 @@ from mutagen.id3 import ID3, APIC, TIT2, TPE1, TALB, USLT  # error
 
 # two args: id  type
 # type=song, lyric, comments, detail, artist, album, search
-# eg  API = 'https://api.imjad.cn/cloudmusic/?type=song&id=1234132'    download music
+# eg  API = 'https://api.imjad.cn/cloudmusic/?type=song&id=1234132'
+
 # APIC: picture
 # TIT2: title
 # TPE1: artist
@@ -25,8 +26,9 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 MSCDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '网易云音乐缓存')
 headers = {'User-agent': 'Mozilla/5.0'}
+
 '''
-1def safeprint(s):
+def safeprint(s):
     #deal with invalid encoded filename
     try:
         print(s)
@@ -48,8 +50,6 @@ class netease_music:
             i for i in os.listdir(path)
             if i.endswith('.uc') or i.endswith('.uc!')
         ]
-        self.id_name = {self.getId(i): i for i in self.files}
-        self.name_id = {j: i for i, j in self.id_name.items()}
         if not os.path.exists(MSCDIR):
             os.mkdir(MSCDIR)
         # self.nameXpath ='//div[@class="tit"]/em[@class="f-ff2"]/text()'
@@ -78,30 +78,28 @@ class netease_music:
         name = artist + ' - ' + title
         for i in '>?*/\\:"|<':
             name = name.replace(i, '-')  # form valid file name
-        self.id_name[musicId] = name
-        #print('''{{title: "{title}",artist: "{artist}",mp3: "http://ounix1xcw.bkt.clouddn.com/{name}.mp3",cover: "{cover}",}},'''\
-        #.format(title = title,name = name,artist=artist,cover=dic['cover'][0]))
-        return os.path.join(MSCDIR, name + '.mp3')
+        return name
 
-    def decrypt(self, name):
-        cachePath = os.path.join(self.path, name)
-        musicId = self.name_id[name]
+    def decrypt(self, rawname):
+        musicId = self.getId(rawname)
         idpath = os.path.join(MSCDIR, musicId + '.mp3')
-        if not os.path.exists(idpath):
-            with open(idpath, 'wb') as f:
-                f.write(bytes(self._decrypt(cachePath)))
+        with open(idpath, 'wb') as f:
+            f.write(bytes(self._decrypt(os.path.join(self.path, rawname))))
         try:  # from web
             info = self.getInfoFromWeb(musicId)
         except Exception as e:  # from file
             #print(e, ' at line {}'.format(sys.exc_info()[-1].tb_lineno))
             info = dict(MP3(path, ID3=EasyID3))
 
-        path = self.getName(info, musicId)
+        name = self.getName(info, musicId)
+
+        path = os.path.join(MSCDIR, name + '.mp3')
         if os.path.exists(path):
-            os.remove(idpath)
-        else:
-            os.rename(idpath, path)
-        return info, path
+            os.remove(path)
+        os.rename(idpath, path)
+        info['lyric'] = [self.getLyric(musicId)]
+        self.setID3(info, path)
+        return name
 
     def _decrypt(self, cachePath):
         with open(cachePath, 'rb') as f:
@@ -117,17 +115,12 @@ class netease_music:
         except:
             return ''
 
-    def setID3(self, lrc, info, path):
+    def setID3(self, info, path):
         tags = ID3(path)
         # remove old unsychronized lyrics
         if len(tags.getall("USLT")) != 0:
             tags.delall("USLT")
 
-        dic = {'title': TIT2, 'artist': TPE1, 'album': TALB}
-
-        for k, T in dic.items():
-            if k in info:
-                tags.add(T(encoding=3, lang='', desc='', text=info[k][0]))
         if ('cover' in info):
             tags.add(
                 APIC(
@@ -138,19 +131,21 @@ class netease_music:
                     data=requests.get(info['cover'][0],
                                       stream=True).raw.read()))
 
-        for key in dic:
-            li = tags[dic[key].__name__].text
-            print('\t{}: {}'.format(key.ljust(6), ', '.join(li) ))
+        dic = {'title': TIT2, 'artist': TPE1, 'album': TALB, 'lyric': USLT}
 
-        tags.add(USLT(encoding=3, text=lrc))
+        for k, T in dic.items():
+            if k in info:
+                tags.add(T(encoding=3, text=info[k][0]))
+        for key in ['title', 'artist', 'album']:
+            li = tags[dic[key].__name__].text
+            print('\t{}: {}'.format(key.ljust(6), ', '.join(li)))
+
         tags.save()
 
     def getMusic(self):
-        for ct, name in enumerate(self.files):
-            info, path = self.decrypt(name)
-            musicId = self.name_id[name]
-            print('[{}]'.format(ct + 1).ljust(5) + self.id_name[musicId])
-            self.setID3(self.getLyric(musicId), info, path)
+        for ct, rawname in enumerate(self.files):
+            name = self.decrypt(rawname)
+            print('[{}]'.format(ct + 1).ljust(5) + name)
 
 
 if __name__ == '__main__':
@@ -162,7 +157,7 @@ if __name__ == '__main__':
     elif platform.startswith('win'):
         path = 'C:/Users/{user}/AppData/Local/Netease/CloudMusic/Cache/Cache'.format(
             user=user)
-    #elif platform.startswith('linux'):
+        #elif platform.startswith('linux'):
     else:  # macpro
         path = '/Users/macbookpro({})/Library/Containers/com.netease.163music/Data/Caches/online_play_cache'.format(
             user=user)
